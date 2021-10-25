4cc2cd83a91d34e5eaf689d202a2b33366b7df0a
Bethel D <88989326+betheld@users.noreply.github.com>
2021-10-25 18:55:43 -0400

    Create database.js

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : "mongodb+srv://betheld:iHygEDvAbztdpdy1@cluster0.ikrip.mongodb.net/roulette?retryWrites=true&w=majority" ,
+    'dbName': 'Roulette'
+};
